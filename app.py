916b04b5107c8ded8ea776c66fb67f8901ce861b
Vladimir Biryukov <vovanjudo@gmail.com>
2025-06-30 23:35:36 +0200

    Enable allow_unsafe_werkzeug option for SocketIO server

--- app.py
+++ app.py
@@ -639,4 +639,4 @@ def handle_disconnect():
     print('Client disconnected')
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
